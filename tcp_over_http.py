d56e9587f8f6f7aac953b8d46cd2136a614c6454
Ming Dai <radaiming@gmail.com>
2016-04-06 21:32:56 +0800

    Add ListenProtocol and SendProtocol to forward TCP packets, but looks they are not necessary, anyway, commit for backup, if I need them later

--- tcp_over_http.py
+++ tcp_over_http.py
@@ -8,6 +8,8 @@ import functools
 import logging
 import os
 import pwd
+import re
+import socket
 import struct
 import subprocess
 import sys
@@ -22,6 +24,83 @@ IFF_TUN = 0x0001
 IFF_NO_PI = 0x1000
 TUNSETOWNER = TUNSETIFF + 2
 DEBUG = True
+PROXY_SERVER = ('127.0.0.1', 7070)
+listen_server_nat_map = {}
+
+
+class SendProtocol(asyncio.Protocol):
+    def __init__(self, transport, remote_peer, block_queue):
+        self.transport = None
+        # use listen_transport to send data back
+        self.listen_transport = transport
+        # a (host, port) tuple
+        self.remote_peer = remote_peer
+        self.block_queue = block_queue
+
+    def connection_made(self, transport):
+        self.transport = transport
+        # start HTTP CONNECT
+        conn_str = 'CONNECT %s:%d HTTP/1.1\r\nAccept: */*\r\n\r\n' % self.remote_peer
+        self.transport.write(conn_str.encode('ascii'))
+
+    def data_received(self, data):
+        if not self.block_queue.empty():
+            # HTTP CONNECT not finished, this is response from proxy server
+            yield from self.block_queue.get()
+            # unblock the 'yield from' in ListenProtocol
+            self.block_queue.task_done()
+            data_str = data.decode('ascii', 'ignore')
+            ret = re.match('HTTP/\d\.\d\s+?(\d+?)\s+?', data_str)
+            if not ret or ret.group()[0] != '200':
+                logging.error('failed to connect to proxy server: ' + data_str)
+                self.transport.close()
+                self.listen_transport.close()
+        else:
+            # the real data we want
+            self.listen_transport.write(data)
+
+
+class ListenProtocol(asyncio.Protocol):
+    """
+    listen for forwarded packet, then call SendProtocol
+    to send them to proxy server
+    """
+    def __init__(self):
+        self.transport = None
+        # use send_transport to send data to proxy server
+        self.send_transport = None
+        # will be used in SendProtocol
+        self.remote_peer = None
+        self.loop = asyncio.get_event_loop()
+        # we need to block all request until HTTP CONNECT success
+        self.block_queue = asyncio.Queue(self.loop)
+        self.block_queue.put('x')
+
+    def connection_made(self, transport):
+        self.transport = transport
+        self.remote_peer = transport.get_extra_info('peername')
+
+    def data_received(self, data):
+        # NAT map is not needed here, each connection
+        # will have an individual coroutine
+        if self.send_transport is None:
+            server, port = PROXY_SERVER
+            # create TCP connection to proxy server
+            self.send_transport, _ = yield from self.loop.create_connection(
+                lambda: SendProtocol(self.transport, self.remote_peer, self.block_queue),
+                server, port)
+        # block until HTTP CONNECT success
+        # FIXME: if SendProtocol failed before proxy server response, will block here forever?
+        yield from self.block_queue.join()
+        self.send_transport.write(data)
+
+
+class ForwardProtocol(asyncio.Protocol):
+    """
+    forward packet to ListenProtocol, let it
+    handle the whole TCP details
+    """
+    pass
 
 
 def switch_user(tun):
@@ -50,16 +129,18 @@ def create_tun():
 
 
 @asyncio.coroutine
-def process_packet(packet):
+def process_packet(packet, listen_port):
+    # modify the packet; then use ForwardProtocol to send
     print(hex(packet[9]))
 
 
-def handle_read(tun, loop):
+def handle_read(tun, listen_port):
     packet = tun.read(MTU)
     if packet[9:10] != b'\x06':
         logging.debug('non TCP packet received, dropping')
         return
-    asyncio.ensure_future(process_packet(packet), loop=loop)
+    loop = asyncio.get_event_loop()
+    asyncio.ensure_future(process_packet(packet, listen_port), loop=loop)
 
 
 def main():
@@ -75,9 +156,14 @@ def main():
     try:
         tun = create_tun()
         switch_user(tun)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind((IP, 0))
+        _, listen_port = sock.getsockname()
 
         loop = asyncio.get_event_loop()
-        loop.add_reader(tun, functools.partial(handle_read, tun, loop))
+        listen_coro = loop.create_connection(ListenProtocol, sock=sock)
+        asyncio.ensure_future(listen_coro)
+        loop.add_reader(tun, functools.partial(handle_read, tun, listen_port))
         loop.run_forever()
     finally:
         loop.close()
